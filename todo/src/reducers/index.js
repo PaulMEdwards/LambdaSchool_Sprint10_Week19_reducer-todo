43729fc1f307ce26f11f333b8e48e095845d9a1d
Paul M Edwards <pauledwards@gmail.com>
2019-11-19 01:23:23 -0800

    MVP + Stretch (edit/delete individual tasks)

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,41 @@
+const newTask = (itemValue, idValue=Date.now()) => {
+  return {
+    id: idValue,
+    item: itemValue,
+    editing: false,
+    completed: false
+  }
+}
+
+export const initialState = [newTask('Learn about reducers')];
+
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return [...state, newTask(action.payload)];
+    case 'REMOVE_TASK':
+      return state.filter(todo => {
+        return !(todo.id === action.payload);
+      });
+    case 'TOGGLE_COMPLETED':
+      return state.map(todo => {
+        if (todo.id === action.payload) {
+          return {...todo, completed: !todo.completed}
+        } else {
+          return todo;
+        }
+      });
+    case 'CLEAR_DONE':
+      console.log('state in reducer: ', state);
+      console.log('state.length in reducer: ', state && state.length);
+      if (state && state.length && state.length > 0) {
+        return state.filter(todo => {
+          return !todo.completed;
+        });
+      } else {
+        return [];  //Fail safe when "Clear Done" clicked too many times.
+      }
+    default:
+      return state;
+  }
+};
